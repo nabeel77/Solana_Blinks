bbd000b2ab1af3d4e13246626c4a9e6018319a23
Nabeel77 <nabeel.beeni30@gmail.com>
2024-08-25 21:47:59 +0300

    changed next configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    source: "/actions.json",
-    headers: [
-      { key: "Access-Control-Allow-Origin", value: "*" },
-      { key: "Content-Type", value: "application/json" },
-    ]
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: "/actions.json",
+          headers: [
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Content-Type", value: "application/json" },
+          ],
+        },
+  
+      ];
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/",
+          destination: "/",
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
